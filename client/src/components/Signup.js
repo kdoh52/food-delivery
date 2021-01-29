02250134575006d233d875e86a157f6380e7ddde
kdoh52 <kdoh52@gmail.com>
2021-01-29 15:22:01 -0800

    sign up user

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,78 +1,163 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Typography, Select, MenuItem, FormHelperText, InputLabel, FormControl } from "@material-ui/core";
-
+import { TextField, Typography, Select, MenuItem, Button, InputLabel, FormControl } from "@material-ui/core";
+import API from '../utils/API'
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
     //   margin: theme.spacing(1),
       minWidth: 167,
+      marginTop: '5px',
     },
     selectEmpty: {
       marginTop: theme.spacing(2),
     },
+    title: {
+        fontSize: 40
+    },
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        // alignItems: 'center',
+        // margin: '0 auto',
+        backgroundColor: '#25ba41',
+        height: '100vh'
+    },
+    child: {
+        // alignItems: 'center',
+        // margin: '0 auto',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        backgroundColor: 'white',
+        width: '200px',
+        padding: 50,
+        paddingTop: 40,
+        borderRadius: 20,
+        margin: 0,
+        position: 'absolute',
+        top: '50%',
+        msTransform: 'translateY(-50%)',
+        transform: 'translateY(-50%)',
+    },
+    button: {
+        color: 'white',
+        backgroundColor: 'black',
+        marginTop: '25px',
+        '&:hover': {
+            backgroundColor: "gray",
+        },
+    },
+    field: {
+        marginTop: '8px'
+    }
 }));
 
 export default function Signup() {
     const classes = useStyles();
-    const [role, setRole] = useState('Ordering');
+    const [role, setRole] = useState('');
+    const [formObject, setFormObject] = useState([]);
+
+    function handleInputChange(e) {
+        const { name, value } = e.target;
+        setFormObject({ ...formObject, [name]: value });
+    }
+
+    function handleRoleChange(event) {
+        setRole(event.target.value);
+        setFormObject({ ...formObject, 'role': role });
+    };
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        console.log(formObject)
+        console.log(role)
+        setFormObject({ ...formObject, 'role': role }, registerUser(formObject));
+        // console.log(formObject)
+        // registerUser(formObject);
+    };
+
+    function registerUser(userData) {
+        // console.log(userData)
+        if (userData.firstName && userData.lastName && userData.email && userData.phone && userData.role) {
+            API.saveUser(userData)
+            .then(console.log('signed up'))
+            .catch((err) => console.log(err));
+        } else {
+            console.log('reject')
+        }
+    }
 
     return (
-        <div>
-            <Typography variant='h2'>
-                Sign up
-            </Typography>
-            <form>
-                <TextField
-                //   variant="filled"
-                margin="normal"
-                required
-                //   fullWidth
-                id="firstName"
-                //   onChange={handleInputChange}
-                name="firstName"
-                //   autoFocus
-                label="First Name"
-                />
-                <br/>
-                <TextField
-                margin="normal"
-                required
-                id="lastName"
-                name="lastName"
-                label="Last Name"
-                />
-                <br/>
-                <TextField
-                margin="normal"
-                required
-                id="email"
-                name="email"
-                label="Email"
-                />
-                <br/>
-                <TextField
-                margin="normal"
-                required
-                id="phone"
-                name="phone"
-                label="Phone"
-                />
-                <br/>
-                <FormControl className={classes.formControl}>
-                    <InputLabel id="demo-simple-select-label">Role</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-filled-label"
-                        id="demo-simple-select-filled"
-                        required
-                        // value={role}
-                        // onChange={handleChange}
-                        >
-                        <MenuItem value="ordering">Ordering</MenuItem>
-                        <MenuItem value="delivering">Delivering</MenuItem>
-                    </Select>
-                </FormControl>
-            </form>
+        <div className={classes.container}>
+            <div className={classes.child}>
+                <Typography className={classes.title} variant='h2'>
+                    Sign up
+                </Typography>
+                <br />
+                <form>
+                    <TextField
+                    //   variant="filled"
+                    className={classes.field}
+                    margin="normal"
+                    required
+                    //   fullWidth
+                    id="firstName"
+                    onChange={handleInputChange}
+                    name="firstName"
+                    //   autoFocus
+                    label="First Name"
+                    />
+                    <br/>
+                    <TextField
+                    className={classes.field}
+                    margin="normal"
+                    required
+                    id="lastName"
+                    onChange={handleInputChange}
+                    name="lastName"
+                    label="Last Name"
+                    />
+                    <br/>
+                    <TextField
+                    className={classes.field}
+                    margin="normal"
+                    required
+                    id="email"
+                    onChange={handleInputChange}
+                    name="email"
+                    label="Email"
+                    />
+                    <br/>
+                    <TextField
+                    className={classes.field}
+                    margin="normal"
+                    required
+                    id="phone"
+                    onChange={handleInputChange}
+                    name="phone"
+                    label="Phone"
+                    />
+                    <br/>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">Role</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-filled-label"
+                            id="demo-simple-select-filled"
+                            required
+                            id="role"
+                            value={role}
+                            onChange={handleRoleChange}
+                            name="role"
+                            label="Role"
+                            >
+                            <MenuItem value="ordering">Ordering</MenuItem>
+                            <MenuItem value="delivering">Delivering</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Button onClick={handleSubmit} className={classes.button} variant="contained">Sign up</Button>
+                </form>
+            </div>
         </div>
     )
 }
--- models/User.js
+++ models/User.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  id: { type: String, required: true },
-  title: { type: String, required: true },
-  year: { type: String, required: true },
-  image: { type: String },
+  // id: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  role: { type: String, required: true },
 });
 
 const User = mongoose.model("User", userSchema);
